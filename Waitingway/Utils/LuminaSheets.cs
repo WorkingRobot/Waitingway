cf16bba993f68c083a716a1a01073d4f5aee5917
Asriel Camora <asriel@camora.dev>
2024-06-14 12:36:09 -0700

    Update dependencies

--- Waitingway/Utils/LuminaSheets.cs
+++ Waitingway/Utils/LuminaSheets.cs
@@ -1,12 +1,12 @@
+using ExdSheets;
 using Lumina.Excel;
-using Lumina.Excel.GeneratedSheets2;
-using LWorld = Lumina.Excel.GeneratedSheets2.World;
+using SWorld = ExdSheets.World;
 
 namespace Waitingway.Utils;
 
 public static class LuminaSheets
 {
-    public static readonly ExcelSheet<LWorld> World = Service.DataManager.GetExcelSheet<LWorld>()!;
+    public static readonly ExcelSheet<SWorld> World = Service.DataManager.GetExcelSheet<SWorld>()!;
     public static readonly ExcelSheet<WorldDCGroupType> WorldDCGroupType = Service.DataManager.GetExcelSheet<WorldDCGroupType>()!;
     public static readonly ExcelSheet<Error> Error = Service.DataManager.GetExcelSheet<Error>()!;
 }
