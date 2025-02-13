83afbfe69ec55c640c46fc7437d09f9c30011566
Asriel Camora <asriel@camora.dev>
2025-02-12 19:21:03 -0800

    Refactor redirects

--- web/src/routes/redirects.rs
+++ web/src/routes/redirects.rs
@@ -1,25 +1,28 @@
 use crate::config::Config;
-use actix_web::{dev::HttpServiceFactory, get, http::header, web, HttpResponse, Result};
+use actix_web::{
+    dev::HttpServiceFactory,
+    get,
+    http::header,
+    web::{self, Redirect},
+    HttpResponse, Responder, Result,
+};
 
 pub fn service() -> impl HttpServiceFactory {
     (discord, funding, github)
 }
 
 #[get("/discord/")]
-async fn discord(config: web::Data<Config>) -> Result<HttpResponse> {
-    Ok(HttpResponse::MovedPermanently()
-        .insert_header((
-            header::LOCATION,
-            format!("https://discord.gg/{}", config.discord.guild_invite_code),
-        ))
-        .finish())
+async fn discord(config: web::Data<Config>) -> Result<impl Responder> {
+    Ok(Redirect::to(format!(
+        "https://discord.gg/{}",
+        config.discord.guild_invite_code
+    ))
+    .permanent())
 }
 
 #[get("/funding/")]
-async fn funding() -> Result<HttpResponse> {
-    Ok(HttpResponse::MovedPermanently()
-        .insert_header((header::LOCATION, "https://ko-fi.com/camora"))
-        .finish())
+async fn funding() -> Result<impl Responder> {
+    Ok(Redirect::to("https://ko-fi.com/camora").permanent())
 }
 
 #[get("/github/")]
