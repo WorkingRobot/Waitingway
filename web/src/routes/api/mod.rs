d97fd2362f5d96022acdb7ce3837fa4338fa9d97
Asriel Camora <asriel@camora.dev>
2025-02-24 19:06:45 -0800

    Add error logging on api endpoints

--- web/src/routes/api/mod.rs
+++ web/src/routes/api/mod.rs
@@ -7,7 +7,12 @@ mod summary;
 mod travel;
 mod world_status;
 
-use actix_web::{dev::HttpServiceFactory, web};
+use actix_web::{
+    body::{EitherBody, MessageBody},
+    dev::{HttpServiceFactory, ServiceResponse},
+    middleware::{ErrorHandlerResponse, ErrorHandlers},
+    web::{self, Bytes},
+};
 
 fn v1() -> impl HttpServiceFactory {
     web::scope("/v1")
@@ -25,5 +30,52 @@ fn v2() -> impl HttpServiceFactory {
 }
 
 pub fn service() -> impl HttpServiceFactory {
-    web::scope("/api").service(v1()).service(v2())
+    web::scope("/api").service(v1()).service(v2()).wrap(
+        ErrorHandlers::new()
+            .default_handler_client(|r| log_error(true, r))
+            .default_handler_server(|r| log_error(false, r)),
+    )
+}
+
+fn log_error<B: MessageBody + 'static>(
+    is_client: bool,
+    res: ServiceResponse<B>,
+) -> actix_web::Result<ErrorHandlerResponse<B>> {
+    Ok(ErrorHandlerResponse::Future(Box::pin(log_error2(
+        is_client, res,
+    ))))
+}
+
+async fn log_error2<B: MessageBody + 'static>(
+    is_client: bool,
+    res: ServiceResponse<B>,
+) -> actix_web::Result<ServiceResponse<EitherBody<B>>> {
+    let (req, res) = res.into_parts();
+    let (res, body) = res.into_parts();
+
+    let body = {
+        let data = actix_web::body::to_bytes_limited(body, 1 << 12).await;
+        let line = match &data {
+            Ok(Ok(data)) => String::from_utf8_lossy(data).into_owned(),
+            Ok(Err(_)) => "Error reading body".to_string(),
+            Err(_) => "Body too large".to_string(),
+        };
+        if is_client {
+            log::error!("Client Error: {}", line);
+        } else {
+            log::error!("Server Error: {}", line);
+        }
+
+        match data {
+            Ok(Ok(bytes)) => bytes,
+            Ok(Err(_)) => Bytes::from_static(b"Body conversion failure"),
+            Err(_) => Bytes::from_static(b"Body too large"),
+        }
+    };
+
+    let res = ServiceResponse::new(req, res.map_body(|_head, _body| body))
+        .map_into_boxed_body()
+        .map_into_right_body();
+
+    Ok(res)
 }
